7686721ac1d0b7dd106cb7659c55252cb38fe050
Robin Ochieng <robinochieng73@gmail.com>
2024-12-09 16:44:51 +0300

    Updated the data processing  file

--- modules/data_processing.R
+++ modules/data_processing.R
@@ -2,8 +2,6 @@
 
 read_and_process_data_motor <- function(filepath) {
   Data <- read_excel(filepath, col_types = c("date", "text", "text", "text", "text", "text", "text", "text", "text"))
-
-  
   return(Data)
 }
 
@@ -12,16 +10,12 @@ read_and_process_data_motor <- function(filepath) {
 
 read_and_process_data_medical <- function(filepath) {
   Data <- read_excel(filepath, col_types = c("date",  "numeric", "numeric", "text", "text", "text", "text", "text", "text", "text", "text"))
-
-  
   return(Data)
 }
 
 
 read_and_process_data_drysalesmotor <- function(filepath) {
   Data <- read_excel(filepath, sheet = "MOTOR", col_types = c("date", "text", "text", "text", "numeric", "numeric", "numeric", "text", "text", "text"))
-
-  
   return(Data)
 }
 
@@ -29,8 +23,6 @@ read_and_process_data_drysalesmotor <- function(filepath) {
 
 read_and_process_data_drysalesmedical <- function(filepath) {
   Data <- read_excel(filepath, sheet = "MEDICAL", col_types = c("date", "text", "text", "text", "numeric", "numeric", "numeric", "text", "text", "text"))
-
-  
   return(Data)
 }
 
@@ -38,9 +30,9 @@ read_and_process_data_drysalesmedical <- function(filepath) {
 
 
 read_and_process_data_claims <- function(filepath) {
-  Data <- read_csv(filepath, show_col_types = FALSE)
-
-  
+  Data <- read_csv(filepath, col_types = cols(
+        `Date of Loss` = col_date(format = "%m/%d/%Y"), 
+        `Reported On` = col_date(format = "%m/%d/%Y")))
   return(Data)
 }
 
